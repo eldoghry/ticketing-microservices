22b42829b479c9d7657d5299a0ad55ff0dd18a29
Mohamed Magdy <mohamed.eldoghry@contact.eg>
2024-06-08 13:19:40 +0300

    add morgan

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,22 @@
 import express, { Express, Request, Response } from "express";
+import morgan from "morgan";
 
 const app: Express = express();
 app.use(express.json());
+const PORT = 4001;
 
-app.get("/", (req: Request, res: Response) => {
+app.use(morgan("dev"));
+
+app.get("/api/users", (req: Request, res: Response) => {
+  console.log("Hello World");
   res.send("Hello World");
 });
 
-app.listen(4001, () => {
-  console.log("Example app listening on port 3000!!");
+app.get("/", (req: Request, res: Response) => {
+  console.log("Hello World//");
+  res.send("Hello World //");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
