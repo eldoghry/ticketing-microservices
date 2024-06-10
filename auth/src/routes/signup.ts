a6bf5a4b4a746db7abaa869aacad7e31d12d0946
Mohamed Magdy <mohamed.eldoghry@contact.eg>
2024-06-10 14:00:15 +0300

    refactor signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from "express-validator";
 import { ValidationException } from "../errors/validation-error";
 import { BadRequestException } from "../errors/badRequest-error";
 import { User } from "../models/user";
+import JwtToken from "../services/jwt";
 
 const router = express.Router();
 
@@ -26,14 +27,16 @@ router.post(
 
     // 2) check if email already used before
     const user = await User.findOne({ email });
-    if (user) throw new BadRequestException("Email is already in use");
+    // if (user) throw new BadRequestException("Email is already in use");
 
     // 3) hash password & create user
     const result = await User.build({ email, password }).save();
 
     // 4) create jwt token
-    // 5) set cookies with jwt
+    const jwt = JwtToken.signToken({ id: result.id, email: result.email });
 
+    // 5) set cookies with jwt
+    req.session = { ...req.session, jwt };
     res.status(201).send(result);
   }
 );
