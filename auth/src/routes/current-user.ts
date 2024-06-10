7fdfde891cca6d8e339a2dde2c2325f2776452f3
Mohamed Magdy <mohamed.eldoghry@contact.eg>
2024-06-10 13:59:58 +0300

    current user logic

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from "express";
+import { UnauthorizedException } from "../errors/unuthorized-error";
+import JwtToken from "../services/jwt";
 
 const router = express.Router();
 
-router.post("/api/users/current", (req: Request, res: Response) => {
-  res.send("current user");
+router.get("/api/users/current", (req: Request, res: Response) => {
+  // extract jwt from user and validate it
+  if (!req.session?.jwt) throw new UnauthorizedException();
+
+  const jwt = req.session?.jwt;
+  const user = JwtToken.verifyToken(jwt);
+
+  res.send(user);
 });
 
 export { router as currentUserRouter };
