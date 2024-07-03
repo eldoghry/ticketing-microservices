b057508ab115766a673fad857be96c88308f3fc8
Mohamed Magdy <mohamed.eldoghry@contact.eg>
2024-07-03 19:11:40 +0300

    navbar header

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,8 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import Header from '../component/header'
 
 
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return <>
+        <Header currentUser={pageProps.currentUser} />
+        <Component {...pageProps} />
+    </>
+
 }
 export default MyApp
